552c59f83673449d9ed7c7f6e54c8e71bb98217a
saker <saker.lin@gmail.com>
2026-03-11 21:23:19 +0800

    chore: replace opencc with opencc-js; pin child-process-promise; fix npm audit

--- src/s2t.js
+++ src/s2t.js
@@ -1,35 +1,33 @@
 #!/usr/bin/env node
-const OpenCC = require('opencc')
+const OpenCC = require('opencc-js')
 const program = require('commander')
 const fs = require('fs')
- 
 
 program.version('0.0.1').usage('<fileName>').parse(process.argv)
 
-let text
 if (!program.args.length) {
   program.help()
 } else {
   // 從參數讀入檔名
   const file = `${process.cwd()}/${program.args}`
-  let filename = `${program.args}`.split('.')
+  const filename = `${program.args}`.split('.')
   const prefix = filename[0]
-   
+
   const ouputFileName = prefix + '_S2T.txt'
   if (fs.existsSync(file)) {
     // 讀取原始文字檔
     fs.readFile(file, function (err, data) {
-      if (!err) {
-        const converter = new OpenCC('s2t.json');
+      if (err) return console.log(err)
 
-        converter.convertPromise(data).then(converted => { 
-						fs.writeFile(ouputFileName, converted, function (err) {
-							if (err) {
-								console.log(err)
-							}
-						})
-        });
-      }
+      // Simplified (cn) -> Traditional (tw)
+      const converter = OpenCC.Converter({ from: 'cn', to: 'tw' })
+      const converted = converter(data.toString('utf8'))
+
+      fs.writeFile(ouputFileName, converted, function (err) {
+        if (err) {
+          console.log(err)
+        }
+      })
     })
   } else {
     console.log('File not exist!!!')
